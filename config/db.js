421d96c299fb34e9a26b5e77984c28d3e8cdf6d0
Angel <angel333gomez@gmail.com>
2021-06-13 16:24:44 -0400

    Configured Backend 1

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+       useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connects')
+
+  } catch(err) {
+
+    console.log(err.message)
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
