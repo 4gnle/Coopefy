09adf66e0e185b8929ca8d6be6966436a17c5d01
Angel <angel333gomez@gmail.com>
2021-06-16 21:07:35 -0400

    Added Register

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const {check, validationResult} = require('express-validator')
+
+const User = require('../../models/Users')
 
 // @router GET api/auth
 // @desc Register User
@@ -10,15 +13,47 @@ router.post('/', [
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Write a valid email').isEmail(),
   check('password', 'You need a secure password (more than 8 characters)').isLength({ min: 8})
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()){
     return res.status(400).json({errors: errors.array() });
   }
 
-  console.log(req.body);
-  res.send('Users Registered');
+  // This breaks down the req.body (data received)
+  const {name, email, password} = req.body;
+
+  try {
+    // Finding registered emails
+    let user = await User.findOne({email});
+
+    if (user) {
+      res.status(400).json({ errors: [{msg: 'Email is already registered'}]})
+    }
+
+    // Hashing passwords
+    user = new User({
+      name,
+      email,
+      password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    //Sending the JWT token
+
+    console.log(req.body);
+    res.send('User Registered');
+
+  } catch(err) {
+
+    console.log(err.message);
+    res.status(500).send('Registering Error');
+  }
+
 });
 
 // @router GET api/auth
