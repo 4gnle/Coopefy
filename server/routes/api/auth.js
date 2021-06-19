4e4652624f0e32c3d30da6f7ab14daa3b9a0d346
Angel <angel333gomez@gmail.com>
2021-06-19 16:04:07 -0400

    Wasted Day

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/Users')
 
@@ -19,6 +23,72 @@ router.get('/', auth, async (req, res) => {
   }catch(err){
     res.status(500).send('Server error')
     console.error(err.message)
+  }
+});
+
+//@router POST api/auth
+//@desc Auth User & token
+//@access Public
+
+router.post('/', [
+  check('email', 'Write a valid email').isEmail(),
+  check('password', 'Write a valid password').exists()
+], async (req, res) => {
+
+  // Variable takes errors from ValidationResult
+  const errors = validationResult(req);
+
+  //If there are errors, show a message
+  if (!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() });
+  }
+
+  // This breaks down the req.body (data received)
+  const {email, password} = req.body;
+
+  try {
+    // Finding registered emails
+    let user = await User.findOne({email})
+    if (!user) {
+      return  res.status(400).json({ errors: [{msg: 'Invalid Credentials'}]})
+    }
+
+    // Breaking down user
+    user = new User({
+      email,
+      password
+    });
+
+    //Validating the password
+    const isMatch = await bcrypt.compare(req.body.password, user.password)
+
+    if (!isMatch) {
+      return  res.status(401).json({ errors: [{msg: 'Invalid Credentials'}]})
+    }
+    //Sending the JWT token
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {expiresIn: 360000},
+      (err, token) => {
+      if(err) throw err;
+      res.json({ token })
+    });
+
+
+    // console.log(req.body);
+    // res.send('User Registered');
+
+  } catch(err) {
+
+    console.log(err.message);
+    res.status(500).send('Auth error');
   }
 });
 
