c04affcbb63057544ed38890d0c4608a799a811a
Angel <angel333gomez@gmail.com>
2021-06-28 14:00:27 -0400

    Building the Redux

---
+++ client/src/redux/reducers/alert.js
@@ -0,0 +1,15 @@
+import { combineReducers} from 'redux';
+import {SET_ALERT, REMOVE_ALERT} from '../actions/types'
+
+const initialState = [];
+
+export default function(state = initialState, action) {
+  const {payload, type} = action;
+
+  switch(type){
+    case SET_ALERT:
+      return [...state, payload]
+    case REMOVE_ALERT:
+      return [state.filter(alert => alert.id !== payload)]
+  }
+}
