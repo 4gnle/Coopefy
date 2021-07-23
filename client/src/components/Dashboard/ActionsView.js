7f06eae206c8c1f6a241f804048d71a12350565b
Angel <angel333gomez@gmail.com>
2021-07-23 19:50:34 -0400

    Building Edit-Profile Component 30

---
+++ client/src/components/Dashboard/ActionsView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// UI
+import Button from '../UI/Button'
+
+const ActionsView = () => {
+  return (
+    <div className='av-box'>
+      <div className='av-buttons'>
+        <Button></Button>
+      </div>
+    </div>
+  )
+}
+
+export default ActionsView
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,29 +1,35 @@
 import React from 'react'
 
+//Components
 import DashboardFunctions from './DashboardFunctions'
+import ActionsView from './ActionsView'
+
 // import Button from '../UI/Button'
 
 const Dashboard = (history) => {
 
   return (
     <div className='dashboard'>
-      <div className='db-profile-description'>
       <h1>Dashboard</h1>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
+      <div className='actions-view'>
+        <ActionsView />
       </div>
-      <div>
+        <div className='db-profile-description'>
+
+
+          <div>
+          </div>
+          <div>
+          </div>
+        </div>
         <div>
+          <div>
+          </div>
+          <div>
+          </div>
         </div>
         <div>
         </div>
-      </div>
-      <div>
-      </div>
       <DashboardFunctions/>
     </div>
   )
