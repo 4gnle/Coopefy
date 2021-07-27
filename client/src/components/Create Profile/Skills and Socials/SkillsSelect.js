ed189d2f30c072b9dafc8c58b30f827574b5366c
Angel <angel333gomez@gmail.com>
2021-07-27 19:32:02 -0400

    Building Dashboard Component 12

--- client/src/components/Create Profile/Skills and Socials/SkillsSelect.js
+++ client/src/components/Create Profile/Skills and Socials/SkillsSelect.js
@@ -7,32 +7,91 @@ import './SkillsSelect.css'
 import Button from '../../UI/Button'
 
 const SkillsSelect = (props) => {
+
+  const skills = [
+      {id: 'Marketing', name: 'Marketing'},
+      {id: 'Content Writing', name: 'Content Writing'},
+      {id: 'SEO', name: 'SEO'},
+      {id: 'Web Development', name: 'Web Development'},
+      {id: 'Solidity', name: 'Solidity'},
+      {id: 'Community', name: 'Community'},
+      {id: 'VR/AR', name: 'VR/AR'},
+      {id: 'Social Media', name: 'Social Media'},
+      {id: 'UI/UX', name: 'UI/UX'}
+  ]
+
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+
+  const filterPosts = (skills, query) => {
+      if (!query) {
+          return skills;
+      }
+
+      return skills.filter((skill) => {
+          const skillName = skill.name.toLowerCase();
+          return skillName.includes(query);
+      });
+  };
+
+  const addSKills = () => {
+    console.log(skills)
+  }
+
   return (
   <div>
-    <div className='backdrop'           onClick={props.unSelectSkills}
+    <div className='backdrop' onClick={props.unSelectSkills}
     >
     </div>
 
       <div className='select-skills'>
         <div className='skills-window'>
-        <header>Select Skills</header>
-        <select>
-          <option value='Marketing'>Marketing</option>
-          <option value='SEO'>SEO</option>
-          <option value='Web Development'>Web Development</option>
-          <option value='Content Writing'>Content Writing</option>
-          <option value='Solidity'>Solidity</option>
-          <option value='Community'>Community</option>
-          <option value='VR/AR'>VR/AR</option>
-          <option value='Social Media'>Social Media</option>
-          <option value='UI/UX'>UI/UX</option>
-        </select>
+        <header>Choose Your Skills</header>
 
-        <Button
-          onClick={props.unSelectSkills}
-        >
-        OK
-        </Button>
+        <form className='skills-form' action="/" method="get">
+          <label style={{marginBottom: '0px'}}>
+          Search Skills
+          </label>
+          <input
+            className='skills-search'
+            type="text"
+            id="header-search"
+            placeholder="Write your desired skill"
+            name="s"
+          />
+          {' '}
+          <Button
+            className='small'
+            type="submit">
+            Search
+          </Button>
+        </form>
+
+        <div className='skills-list'>
+          {skills.map((skill) => (
+             <button
+              className='skills-badge'
+              key={skill.id}>{skill.name}
+
+            </button>
+          ))}
+        </div>
+
+        <div className='skills-buttons'>
+          <Button
+            className='primary'
+            onClick={addSKills}
+          >
+            Add Skills
+          </Button>
+          <Button
+            className='bad'
+            onClick={props.unSelectSkills}
+          >
+            Cancel
+          </Button>
+        </div>
+
       </div>
     </div>
   </div>
