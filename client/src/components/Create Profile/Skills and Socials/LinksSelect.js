c356fcadb0038f7741d022bdbd5b4242d818b99c
Angel <angel333gomez@gmail.com>
2021-07-29 19:58:02 -0400

    Building Profile-Edit Component 35

--- client/src/components/Create Profile/Skills and Socials/LinksSelect.js
+++ client/src/components/Create Profile/Skills and Socials/LinksSelect.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 //UI and Styles
 import './LinksSelect.css'
+import Button from '../../UI/Button'
 
 const LinksSelect = (props) => {
   return (
@@ -11,35 +12,47 @@ const LinksSelect = (props) => {
 
       <div className='links-select'>
         <div className='links-box'>
+        <header>Add Links</header>
+
           <div className='links-inputs'>
-
-          <header>Add Links</header>
-
-          <i className="fab fa-product-hunt"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-github-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-twitter-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-facebook-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-instagram-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-behance-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-dribbble-square"></i>
-          <input></input>
-          <br></br>
-          <i className="fab fa-linkedin-square"></i>
-          <input></input>
-
+            <small>Add your ID or Username only</small>
+            <br></br>
+            <i className="fab fa-product-hunt"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-github-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-twitter-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-behance-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-dribbble-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-facebook-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-instagram-square"></i>
+            <input></input>
+            <br></br>
+            <i className="fab fa-linkedin-square"></i>
+            <input></input>
           </div>
+
+            <div className='links-buttons'>
+              <Button
+                className='primary'>
+                Add Links
+              </Button>
+              <Button
+                className='bad'
+                onClick={props.unSelectLinks}>
+                Cancel
+              </Button>
+            </div>
         </div>
       </div>
   </div>
