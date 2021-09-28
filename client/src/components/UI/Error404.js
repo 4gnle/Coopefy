da73b5e7254af03b0e61a513d35f18c27c84e42c
Angel <angel333gomez@gmail.com>
2021-09-27 23:34:35 -0300

    Finished Error404

--- client/src/components/UI/Error404.js
+++ client/src/components/UI/Error404.js
@@ -7,6 +7,7 @@ import errorimage from './Error404.png'
 const Error404 = () => {
   return (
     <div className='error-404'>
+      <h1>There's nothing here!</h1>
       <img src={errorimage}/>
     </div>
   )
