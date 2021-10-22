35a638b68ed9ac415e89c13452390be17b4aabc9
Angel <angel333gomez@gmail.com>
2021-10-22 14:14:36 -0300

    Fixed Project Page

--- client/src/components/Projects/Project/ProjectPage.js
+++ client/src/components/Projects/Project/ProjectPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 //UI & CSS
-import "./ProjectPage.css";
-import styled from 'styled-components';
+import styled from "styled-components";
 import Spinner from "../../UI/Spinner";
 import Button from "../../UI/Button";
 import Error404 from "../../UI/Error404";
@@ -16,38 +15,40 @@ import usdc from "../../UI/crypto-icons/usdc.svg";
 //Redux & Router
 import {
   getProjectById,
-  getApplicationsbyID,
+  getApplicationsbyID
 } from "../../../redux/actions/project";
 import { getProfile } from "../../../redux/actions/profile";
-import { connect } from "react-redux";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 //Components
 import Apply from "../Applications/Apply";
 import ApplicationList from "../Applications/ApplicationList";
 
-
-const Project = ({
-  project: { project, loading, applications },
-  profile: { signedprofile, user },
-  authenticate: {isAuth, userData},
-  match,
-  history,
-  getProjectById,
-  getApplicationsbyID,
-}) => {
+const Project = ({match, history}) => {
   const [loadProject, setLoadProject] = useState(false);
   const [loadedProjects, setLoadedProjects] = useState(false);
   const [profileLoaded, setProfileLoaded] = useState();
 
   const [applicationBox, setApplicationBox] = useState(false);
 
+  const dispatch = useDispatch();
+
+  const projectData = useSelector(state => state.project);
+  const profileData = useSelector(state => state.profile);
+  const authData = useSelector(state => state.authenticate);
+
+  const {project, applications
+  } = projectData;
+  const {signedprofile, user} = profileData;
+  const { isAuth, userData} = authData;
+
   const getAllData = async () => {
-    await getProfile();
+    await dispatch(getProfile());
     setProfileLoaded(true);
-    await getProjectById(match.params.id);
+    await dispatch(getProjectById(match.params.id));
     setLoadProject(true);
-    await getApplicationsbyID(match.params.id);
+    await dispatch(getApplicationsbyID(match.params.id));
     setLoadedProjects(true);
   };
 
@@ -112,99 +113,96 @@ const Project = ({
           projectname={match.params.projectname}
         />
       )}
-        {!loadProject ? (
-          <Spinner />
-        ) : (
-          <>
-            {project ? (
-              <ProjectBox>
-                <Button className="button bad" onClick={goBack}>
-                  Go Back
-                </Button>
-                <Title>
-                  <h2>{project.projectname}</h2>
-                </Title>
-                <BreakLine />
+      {!loadProject ? (
+        <Spinner />
+      ) : (
+        <>
+          {project ? (
+            <ProjectBox>
+              <Button className="button bad" onClick={goBack}>
+                Go Back
+              </Button>
+              <Title>
+                <h2>{project.projectname}</h2>
+              </Title>
+              <BreakLine />
 
-                <Details>
-                  {project.projectreward && project.projectamount ? (
-                      <p>
-                        <Icon alt="coin icon" src={projecticon()} width="21px" />{" "}
-                        {project.projectamount} {project.projectreward}
-                      </p>
-                  ) : null}
-                    {project.projectlocation ? (
-                      <DetailsLine>
-                        <i class="fas fa-map-marker-alt"></i>{" "}
-                        {project.projectlocation}
-                      </DetailsLine>
-                    ) : (
-                      <DetailsLine>
-                        <i className="fas fa-globe location-icon"></i> Remote
-                      </DetailsLine>
+              <Details>
+                {project.projectreward && project.projectamount ? (
+                  <p>
+                    <Icon alt="coin icon" src={projecticon()} width="21px" />{" "}
+                    {project.projectamount} {project.projectreward}
+                  </p>
+                ) : null}
+                {project.projectlocation ? (
+                  <DetailsLine>
+                    <i class="fas fa-map-marker-alt"></i>{" "}
+                    {project.projectlocation}
+                  </DetailsLine>
+                ) : (
+                  <DetailsLine>
+                    <i className="fas fa-globe location-icon"></i> Remote
+                  </DetailsLine>
+                )}
+                {project.projectduration && (
+                  <DetailsLine>
+                    <i class="far fa-clock"></i> {project.projectduration}
+                  </DetailsLine>
+                )}
+              </Details>
+              <BreakLine />
+              <Description>
+                {project.projectdescription && project.projectdescription}
+              </Description>
+              <BreakLine />
+              <Skills>
+                {project.projectskills.map((skill, index) => (
+                  <div key={index}>
+                    <Button className="show"> {skill}</Button>
+                  </div>
+                ))}
+              </Skills>
+
+              <BottomSection>
+                {signedprofile &&
+                isAuth &&
+                signedprofile.user._id !== userData._id ? (
+                  <Button className="button primary" onClick={toApplication}>
+                    Apply
+                  </Button>
+                ) : (
+                  <>
+                    {!signedprofile && (
+                      <Link to="/register">
+                        <Button className="button primary">
+                          Register to Apply
+                        </Button>
+                      </Link>
                     )}
-                    {project.projectduration && (
-                      <DetailsLine>
-                        <i class="far fa-clock"></i> {project.projectduration}
-                      </DetailsLine>
-                    )}
-                </Details>
-                <BreakLine />
-                <Description>
-                  {project.projectdescription && project.projectdescription}
-                </Description>
-                <BreakLine />
-                <div className="pp-skills">
-                  {project.projectskills.map((skill, index) => (
-                      <div key={index}>
-                        <Button className="show"> {skill}</Button>
-                      </div>
-                  ))}
-                </div>
-
-                <div className="pp-bottom-section">
-                  {signedprofile && isAuth && signedprofile.user._id !== userData._id ? (
-                    <Button className="button primary" onClick={toApplication}>
-                      Apply
-                    </Button>
-                  ) : <>{!signedprofile &&
-                  <Link to='/register'><Button
-                  className="button primary">
-                    Register to Apply
-                  </Button></Link>}</> }
-                </div>
-
-              </ProjectBox>
-            ) : (
-              <Error404 />
-            )}
-          </>
-        )}
-          {loadedProjects && project && (
-            <ApplicationList
-            applications={applications}
-            project={project} />
+                  </>
+                )}
+              </BottomSection>
+            </ProjectBox>
+          ) : (
+            <Error404 />
           )}
+        </>
+      )}
+      {loadedProjects && project && (
+        <ApplicationList applications={applications} project={project} />
+      )}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  project: state.project,
-  profile: state.profile,
-  authenticate: state.authenticate
-});
-export default connect(mapStateToProps, {
-  getProjectById,
-  getProfile,
-  getApplicationsbyID,
-})(Project);
+export default Project;
 
 const ProjectBox = styled.div`
   box-sizing: border-box;
   border: 0px solid #000000;
   box-sizing: border-box;
-  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25), 12px 12px 12px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25),
+    12px 12px 12px rgba(0, 0, 0, 0.25);
   border-radius: 18px;
   width: 90%;
   margin-left: auto;
@@ -221,17 +219,20 @@ const ProjectBox = styled.div`
     right: 0;
     margin: 0;
   }
-`
+`;
 
 const BreakLine = styled.hr`
   opacity: 0.25;
-  width: 100.7%;
-  margin-left: -0.5%
-`
+  width: 100%;
 
+  @media (max-width: 650px) {
+    width: 105%;
+    margin-left: -0.5%;
+  }
+`;
 const Title = styled.div`
   margin-left: 5%;
-`
+`;
 const Details = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -240,16 +241,41 @@ const Details = styled.div`
   width: 100%;
   margin-left: 5%;
   margin-top: -5px;
-`
+`;
 const DetailsLine = styled.span`
   position: relative;
   top: 21px;
-`
+`;
 const Icon = styled.img`
   position: relative;
   top: 5px;
-`
+`;
 const Description = styled.div`
   break-line: anywhere;
   margin-left: 5%;
+`;
+const Skills = styled.div`
+  margin-left: 5%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-auto-rows: 2rem;
+  grid-gap: 1rem;
+  width: 90%;
+  padding: 2%;
+
+  @media (max-width: 650px) {
+    margin-left: 5%;
+    display: grid;
+    grid-template-columns: repeat(3, 2fr);
+    grid-auto-rows: 2rem;
+    grid-gap: 1rem;
+    width: 90%;
+    padding: 2%;
+  }
+`;
+
+const BottomSection = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  text-align:center;
 `
